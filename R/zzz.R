e4e267a6dbc1bd15c8e50d28a1062bb7e63dcba0
b-c-r <55611387+b-c-r@users.noreply.github.com>
2021-12-19 09:51:43 +0100

    Package description with Rccp links

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+#' funfit
+#'
+#' Description of your package: TBA
+#'
+#' @docType package
+#' @author Björn C. Rall <bjoern.c.rall@gmail.com>
+#' @import bbmle BH deSolve foreach odeintr Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib funfit
+#' @name funfit
+NULL
