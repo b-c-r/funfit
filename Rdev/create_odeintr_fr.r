fe1375236d70c9f2918f832496e45019c7f2b72d
b-c-r <55611387+b-c-r@users.noreply.github.com>
2021-12-19 10:40:40 +0100

    Odeintr R-code for creating Rccp-code

---
+++ Rdev/create_odeintr_fr.r
@@ -0,0 +1,12 @@
+
+## The Functional Response model code written in cpp as simple string:
+FR <- 'dxdt[0] = -Fmax * pow(x[0],1+q) / (N0 + pow(x[0],1+q)) * P;'
+
+## Automatic creation of the cpp source code using odeintr:
+fr_code <- odeintr::compile_sys("FR", FR,
+                                 pars = c("Fmax","N0","q","P"),
+                                 method = "rk54",
+                                 compile = F)
+
+## Save the cpp code for further documentation:
+cat(fr_code, file = "srcdev/odeintr_fr_code_raw.cpp")
