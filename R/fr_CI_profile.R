c1d29b71a03f7e91cbe1c5f13871efe1da84a580
b-c-r <55611387+b-c-r@users.noreply.github.com>
2021-12-27 11:34:39 +0100

    Added profiling CI wrapper around bbmle confint

---
+++ R/fr_CI_profile.R
@@ -0,0 +1,51 @@
+#' Profiling the confidence intervalls in parallel
+#'
+#' @description
+#'
+#' @param fit The prey/resource items eaten throughout the experimental
+#'     trial.
+#' @param MC Use parallel computing? (F/T)
+#' @param noC If MC = T, how many cores to use?
+#'
+#' @return Returns the profiled confidence intervals.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' data(dfr_vp)
+#'
+#' fit <- fr_fit(
+#'   Neaten = dfr_vp$Neaten,
+#'   Nstart = dfr_vp$Nstart,
+#'   P = rep(1, length(dfr_vp$Neaten)),
+#'   tend = rep(1, length(dfr_vp$Neaten))
+#' )
+#'
+#' CI <- fr_confint(fit = fit)
+#' CI
+#'
+
+fr_CI_profile <- function(fit,
+                       MC = T,
+                       noC = 3
+){
+
+  parms <- c('l10_Fmax','l10_N0','h')
+  cl <- parallel::makeCluster(noC)
+  doParallel::registerDoParallel(cl)
+  ci <- foreach::foreach(i = 1:3,
+                         .combine = 'rbind',
+                         .errorhandling = "pass",
+                         .packages = c("bbmle","funfit")
+  ) %dopar% {
+    bbmle::confint(object = fit,
+                   parm = parms[i],
+                   method = 'uniroot')
+  }
+  parallel::stopCluster(cl)
+  return(ci)
+}
+
+
+
